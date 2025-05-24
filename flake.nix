316d353e991f12bd41ccb605bcf05e53473f5669
omeyemburg <oskar.git@gmx.net>
2025-05-24 10:49:29 +0200

    chore: add nix flake

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "Obsidian plugin development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            nodejs
+            nodePackages.prettier
+            typescript-language-server
+          ];
+
+          shellHook = ''
+            export SHELL=${pkgs.bashInteractive}/bin/bash
+          '';
+        };
+      }
+    );
+}
