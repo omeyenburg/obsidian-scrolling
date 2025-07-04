b56c45ec6f8daf14cbf6cabbc1c4c3be62584dd7
omeyemburg <oskar.git@gmx.net>
2025-07-04 12:57:46 +0200

    fix(scrollbar): allow horizontal scrollbar on MacOS

--- src/scrollbar.ts
+++ src/scrollbar.ts
@@ -1,34 +1,42 @@
-import type { default as ScrollingPlugin } from "./main";
 import { Platform, MarkdownView } from "obsidian";
+import type { default as ScrollingPlugin } from "./main";
 
 export class Scrollbar {
-    plugin: ScrollingPlugin;
+    private plugin: ScrollingPlugin;
 
     private scrolling = false;
     private scrollTimeout: number;
+    private scrollEventSkip = false;
+
     private currentVisibility: "show" | "hide" | "transparent" | null;
     private currentWidth: number | null;
     private currentFileTreeHorizontal = false;
 
     constructor(plugin: ScrollingPlugin) {
-        // Styling scrollbars doesnt work on MacOS.
-        if (Platform.isMacOS) return;
-
         this.plugin = plugin;
         this.updateStyle();
 
+        // Scrollbars styling doesnt work on MacOS.
+        // Toggling file tree scrollbar works.
+        if (Platform.isMacOS) return;
+
+        this.attachScrollHandler();
         plugin.registerEvent(
             plugin.app.workspace.on("active-leaf-change", this.attachScrollHandler.bind(this)),
         );
-
-        this.attachScrollHandler();
     }
 
+    // Not invoked on MacOS.
     private attachScrollHandler() {
         const view = this.plugin.app.workspace.getActiveViewOfType(MarkdownView);
         if (!view) return;
 
+        this.scrollEventSkip = true;
         window.setTimeout(() => {
+            this.scrollEventSkip = false;
+        }, 250);
+
+        window.requestAnimationFrame(() => {
             const scroller =
                 view.contentEl.querySelector(".cm-scroller") ||
                 view.contentEl.querySelector(".markdown-preview-view");
@@ -39,10 +47,15 @@ export class Scrollbar {
                 "scroll",
                 this.scrollHandler.bind(this),
             );
-        }, 50);
+        });
     }
 
+    // Not invoked on MacOS.
     private scrollHandler(): void {
+        if (this.scrollEventSkip) {
+            return;
+        }
+
         window.clearTimeout(this.scrollTimeout);
 
         if (!this.scrolling) {
@@ -57,9 +70,23 @@ export class Scrollbar {
         }, 500);
     }
 
-    updateStyle(): void {
+    public updateStyle(): void {
         // Styling scrollbars doesnt work on MacOS.
-        if (Platform.isMacOS) return;
+        // Only handle horizontal file tree scrollbar.
+        if (Platform.isMacOS) {
+            // Only proceed if state changed.
+            const fileTreeHorizontal = this.plugin.settings.scrollbarFileTreeHorizontal;
+            if (this.currentFileTreeHorizontal == fileTreeHorizontal) return;
+            this.currentFileTreeHorizontal = fileTreeHorizontal;
+
+            if (fileTreeHorizontal) {
+                document.body.addClass("scrolling-filetree-horizontal");
+            } else {
+                document.body.removeClass("scrolling-filetree-horizontal");
+            }
+
+            return;
+        }
 
         let visibility: "show" | "hide" | "transparent" = "show";
         if (this.plugin.settings.scrollbarVisibility == "hide") {
@@ -88,42 +115,41 @@ export class Scrollbar {
 
         this.currentWidth = width;
         this.currentVisibility = visibility;
-        this.currentFileTreeHorizontal == fileTreeHorizontal;
+        this.currentFileTreeHorizontal = fileTreeHorizontal;
 
         this.removeStyle();
 
-        const styles = document.body.classList;
-
         if (fileTreeHorizontal) {
-            styles.add("scrolling-filetree-horizontal");
+            document.body.addClass("scrolling-filetree-horizontal");
         }
 
         if (this.plugin.settings.scrollbarGlobal) {
-            styles.add("scrolling-global");
+            document.body.addClass("scrolling-global");
         } else {
-            styles.add("scrolling-markdown");
+            document.body.addClass("scrolling-markdown");
         }
 
         if (visibility == "hide") {
-            styles.add("scrolling-hidden");
+            document.body.addClass("scrolling-hidden");
         } else if (visibility == "transparent") {
-            styles.add("scrolling-transparent");
+            document.body.addClass("scrolling-transparent");
         }
 
         if (width > 0) {
-            styles.add("scrolling-width");
+            document.body.addClass("scrolling-width");
             document.body.style.setProperty("--scrolling-scrollbar-width", `${width}px`);
         }
     }
 
-    removeStyle(): void {
-        document.body.classList.remove(
+    public removeStyle(): void {
+        document.body.removeClasses([
             "scrolling-global",
             "scrolling-markdown",
             "scrolling-hidden",
             "scrolling-transparent",
             "scrolling-width",
-        );
+            "scrolling-filetree-horizontal",
+        ]);
 
         document.body.style.removeProperty("--scrolling-scrollbar-width");
     }
